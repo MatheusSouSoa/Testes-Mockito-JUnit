3c710dccd738769c03b89da802c1e279e96b5f44
Matheus Soares <matheus.sousoa.dev@gmail.com>
2023-06-19 23:25:57 -0300

    perfil de testes

---
+++ src/main/java/br/com/testesmockito_junit/api/config/LocalConfig.java
@@ -0,0 +1,27 @@
+package br.com.testesmockito_junit.api.config;
+
+import br.com.testesmockito_junit.api.domain.User;
+import br.com.testesmockito_junit.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+@Configuration
+@Profile("local")
+public class LocalConfig {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Bean
+    public void startDB(){
+        User u1 = new User(null,"Matheus", "matheus@email.com", "matheus");
+        User u2 = new User(null,"José", "jose@email.com", "jose");
+
+
+        userRepository.saveAll(List.of(u1,u2));
+    }
+}
