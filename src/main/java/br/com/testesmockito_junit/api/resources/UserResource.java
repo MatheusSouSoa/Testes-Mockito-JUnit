afa74a837230b82b7d348824132f77ea3140a073
Matheus Soares <matheus.sousoa.dev@gmail.com>
2023-06-19 23:06:14 -0300

    classe user userServiceImpl findbyid

--- src/main/java/br/com/testesmockito_junit/api/resources/UserResource.java
+++ src/main/java/br/com/testesmockito_junit/api/resources/UserResource.java
@@ -1,6 +1,7 @@
 package br.com.testesmockito_junit.api.resources;
 
 import br.com.testesmockito_junit.api.domain.User;
+import br.com.testesmockito_junit.api.services.user.interfaces.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserResource {
 
+    @Autowired
+    private IUserService userService;
+
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
-        return ResponseEntity.ok().body(new User(1L, "Matheus", "matheus@email.com", "mathe"));
+        return ResponseEntity.ok().body(userService.findById(id));
     }
 
 }
