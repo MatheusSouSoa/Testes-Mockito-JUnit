6145c0ef77f9fa13ef430f8e056bbf28ea7f7fab
Matheus Soares <matheus.sousoa.dev@gmail.com>
2023-06-19 23:06:04 -0300

    classe user userServiceImpl findbyid

---
+++ src/main/java/br/com/testesmockito_junit/api/services/user/impl/UserServiceImpl.java
@@ -0,0 +1,23 @@
+package br.com.testesmockito_junit.api.services.user.impl;
+
+import br.com.testesmockito_junit.api.domain.User;
+import br.com.testesmockito_junit.api.repositories.UserRepository;
+import br.com.testesmockito_junit.api.services.user.interfaces.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements IUserService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User findById(Long id) {
+        Optional<User> obj= userRepository.findById(id);
+
+        return obj.orElse(null);
+    }
+}
